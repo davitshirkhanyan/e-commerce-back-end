970a06749825637f1d1e5b91bc10b30cfbb98d93
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-13 21:23:43 -0800

    get all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Category.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  }).then(dbProductData => res.json(dbProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
