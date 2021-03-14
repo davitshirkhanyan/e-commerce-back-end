d509ad017c8bd5bb8394a5f1fe53ceb10fad4c94
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-13 21:30:31 -0800

    get one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,7 +6,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Category.findAll({
+  Product.findAll({
     include: [
       {
         model: Category
@@ -26,19 +26,35 @@ router.get('/', (req, res) => {
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    
+    include: [
+      {
+      model: Category
+    },
+    {
+      model: Tag,
+      through: ProductTag
+    }
+  ]
+  }).then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
