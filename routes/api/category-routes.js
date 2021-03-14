2c8998dcf02877f5562c55e459ff0192df032b6a
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-13 20:32:13 -0800

    find one category by its uid=197609(davit) gid=197609 groups=197609 value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,9 +13,23 @@ router.get('/', (req, res) => {
     });
 });
 
+// find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
